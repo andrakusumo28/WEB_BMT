2443789994e9d010ec43f32ac7d779c5526e7aa9
andrakusumo28 <666deville@gmail.com>
2017-02-24 15:27:57 +0700

    push Login form

---
+++ Pages/LOGIN.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8">
+<meta charset="iso-8859-1">
+<title>;)</title>
+<meta name="generator" content="WYSIWYG Web Builder - http://www.wysiwygwebbuilder.com">
+<link href="WEB_BMT_FRONTPAGE.css" rel="stylesheet">
+<link href="LOGIN.css" rel="stylesheet">
+<!-- Insert Google Analytics code here -->
+</head>
+<body>
+<div id="FooterLayer" style="position:absolute;text-align:center;left:0px;bottom:0px;width:100%;height:50px;z-index:7;" title="Footer">
+<div id="FooterLayer_Container" style="width:1366px;position:relative;margin-left:auto;margin-right:auto;text-align:left;">
+</div>
+</div>
+<div id="HeaderLayer" style="position:absolute;text-align:left;left:0px;top:0px;width:100%;height:123px;z-index:8;" title="Header">
+<label for="" id="Label1" onclick="window.location.href='./MenuUtama.php';return false;" style="position:absolute;left:0px;top:24px;width:1345px;height:62px;line-height:62px;z-index:0;">SISTEM INFORMASI PEMBIAYAAN BMT</label>
+</div>
+<div id="container">
+<div id="wb_Image1" style="position:absolute;left:0px;top:125px;width:1366px;height:476px;z-index:9;">
+<img src="images/pbmt04.jpg" id="Image1" alt=""></div>
+<div id="wb_Form1" style="position:absolute;left:378px;top:165px;width:586px;height:386px;z-index:10;">
+<form name="Form1" method="post" action="" enctype="text/plain" id="Form1">
+<input type="text" id="Editbox1" style="position:absolute;left:226px;top:122px;width:277px;height:31px;line-height:31px;z-index:1;" name="Editbox1" value="">
+<input type="password" id="Editbox2" style="position:absolute;left:226px;top:216px;width:277px;height:32px;line-height:32px;z-index:2;" name="Editbox2" value="">
+<label for="" id="Label3" style="position:absolute;left:39px;top:113px;width:148px;height:50px;line-height:50px;z-index:3;">USERNAME :</label>
+<label for="" id="Label4" style="position:absolute;left:39px;top:208px;width:148px;height:50px;line-height:50px;z-index:4;">PASSWORD :</label>
+<input type="submit" id="Button1" name="btnLogin" value="LOGIN" style="position:absolute;left:227px;top:303px;width:140px;height:46px;z-index:5;">
+<input type="reset" id="Button2" name="btnCancel" value="CANCEL" style="position:absolute;left:373px;top:303px;width:140px;height:46px;z-index:6;">
+</form>
+</div>
+<label for="" id="Label2" style="position:absolute;left:386px;top:179px;width:580px;height:62px;line-height:62px;z-index:11;">LOGIN</label>
+</div>
+</body>
+</html>
